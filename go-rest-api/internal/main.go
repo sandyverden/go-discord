7ed3585526c68ab83fd9a886471318c61aa49d20
sandyverden <sandyverden@gmail.com>
2021-08-07 14:18:20 +0700

    fix typo

--- go-rest-api/internal/main.go
+++ go-rest-api/internal/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
-	"github.com/aws/smithy-go/middleware"
 	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
 	"github.com/sandyverden/go-discord/go-rest-api/pkg/swagger/server/restapi"
 	"github.com/sandyverden/go-discord/go-rest-api/pkg/swagger/server/restapi/operations"
 )
@@ -36,39 +35,35 @@ func main() {
 	api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
 
 	//Start server which listening
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+}
 
-	//Health route return OK
-	func  Health(return operations.NewCheckHealthParams moddleware.Responder {
-		return operations.NewCheckHealthOK().withPayload("OK")
+//Health route return OK
+func Health(operations.CheckHealthParams) middleware.Responder {
+	return operations.NewCheckHealthOK().WithPayload("OK")
+}
+
+//GetHelloUser return hello + your name
+func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
+	return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+}
+
+//GetGopherByName returns a gopher in png
+func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
+	var URL string
+	if gopher.Name != "" {
+		URL = "https://github.com/scraly/gophers/raw/main" + gopher.Name + ".png"
+	} else {
+		// by default we return dr who gopher
+		URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
 	}
 
-	//GetHelloUser return hello + your name
-	func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
-		return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+	response, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("err")
 	}
 
-	//GetGopherByName returns a gopher in png
-	func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
-		var URL string
-		if gopher.Name != "" {
-			URL = "https://github.com/scraly/gophers/raw/main" + gopher.Name + ".png"
-		} else {
-			// by default we return dr who gopher
-			URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
-		}
-
-		response, err := http.Get(URL)
-		if err != nil {
-			fmt.Println("err")
-		}
-
-		return operations.NewGetGopherNameOK().WithPayload(response.Body)
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	log.Println("Listening on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	return operations.NewGetGopherNameOK().WithPayload(response.Body)
 }
